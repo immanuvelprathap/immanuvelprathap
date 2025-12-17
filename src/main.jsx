1ff26b02a7088dfedcb4def746b00d3ce2c77370
Immanuvel Prathap S <mailtoimmanuvel@gmail.com>
2025-12-17 20:09:42 +0530

    Final 17-12-2025

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,15 @@ import App from './App.jsx'
 import GlobalStyles from './styles/GlobalStyle.js';
 import Typography from './styles/Typography.js';
 
+// --- ADD THIS BLOCK --- refresh issue
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -14,3 +23,20 @@ createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+// import React from 'react';
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import GlobalStyles from './styles/GlobalStyle.js';
+// import Typography from './styles/Typography.js';
+
+
+// createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <GlobalStyles />
+//     <Typography />
+//     <App />
+//   </React.StrictMode>,
+// )
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './', // Changed from '/' to './' (relative path)
   
 })
